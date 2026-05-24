2ca6d9f804590f21aaa17ef125ac717b50a4a101
asm-saim <saim777m@gmail.com>
2026-05-24 23:59:21 +0600

    for-of-loop added

---
+++ 1.for-of-loop.js
@@ -0,0 +1,33 @@
+// array er modde jot o gulo upadan ase, prottek ta k niye niye ektar ekbar kkore print korai holo for loop er kaj.
+// for him , for her etc.
+
+// Basic Structure:
+/* 
+for()
+    {
+
+    }  
+*/
+
+// for(const egg of eggs)
+// {
+//     console.log(egg);
+// }
+
+// Ex:
+
+const balls=[23,45,2,45,77,88];
+console.log(balls);
+
+for(const ball of balls)
+    {
+        console.log(ball);
+    } 
+
+// Ex 2
+
+const fruits=["orange", "Pineapple", "mango", 'berry', "guava"]
+for(const fruit of fruits)
+{
+    console.log(fruit);
+}
