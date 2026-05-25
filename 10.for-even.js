fad01cb894aaaed901ebddcf2afdbbfc95d77e53
asm-saim <saim777m@gmail.com>
2026-05-25 14:38:27 +0600

    for-even for-odd practice

---
+++ 10.for-even.js
@@ -0,0 +1,23 @@
+// // Even 
+// for(let i=2; i<=20; i=i+2)
+// {
+//     console.log(i);
+// }
+
+// // type 2
+// for(let i=2; i<=20; i++)
+// {
+//     if(i%2===0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+// type 3
+for(let i= 1; i<=20; i++)
+{
+    if(i%2!==1)
+    {
+        console.log(i);
+    }
+}
---
+++ 10.for-odd.js
@@ -0,0 +1,25 @@
+// Odd
+
+for(let i = 1; i<=20;i++)
+{
+    if(i%2===1)
+    {
+        console.log(i);
+    }
+}
+
+// type-2
+for(let i = 1; i<=20;i++)
+{
+    if(i%2!==0)
+    {
+        console.log(i);
+    }
+}
+
+// type 3
+for(let i =1; i<=20; i+=2)
+{
+    console.log(i);
+}
+
