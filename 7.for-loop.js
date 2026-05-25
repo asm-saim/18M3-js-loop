6c85ba90c4a21fb01daae97db4b4ed7d9f282f5b
asm-saim <saim777m@gmail.com>
2026-05-25 07:30:43 +0600

    for-loop added

---
+++ 7.for-loop.js
@@ -0,0 +1,45 @@
+// for loop
+/* 
+num = 1;
+while(num<=5)
+{
+    console.log(num);
+    num++;
+}
+*/
+
+/* 
+for(first part : second part : third part)
+{
+    loop statement;
+} 
+*/
+// for(loop variable declaration ; loop condition; increment/decrement)
+// {
+
+// }
+
+/* 
+num = 1;
+while(num<=5)
+{
+    console.log(num);
+    num++;
+}
+*/
+for(number=1;number<=5;number++)
+{
+    console.log(number);
+}
+
+// even
+for(let i =0 ; i<=10; i=i+2)
+{
+    console.log(i);
+}
+
+// odd
+for(let i = 1; i<=10; i=i+2)
+{
+    console.log(i);
+}
