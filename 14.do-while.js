421a8f001a7625ce04d9cfb72d6b5206bfd3f3f1
asm-saim <saim777m@gmail.com>
2026-05-25 18:46:32 +0600

    do-while added

---
+++ 14.do-while.js
@@ -0,0 +1,14 @@
+// while
+let i=10;
+while(i<5){
+    console.log(i);
+    i++;
+}
+
+// do-while: condition jai hok , ekbar se print korbei.
+let n=10;
+do{
+    console.log(n);
+    n++;
+}
+while(n<5)
